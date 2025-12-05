e49ea21d64b383b57d82d92656ccb80190e34007
eggaar <eyepez174@gmail.com>
2025-12-05 11:44:51 -0400

    Delete proyecto_clinica directory

--- proyecto_clinica/backend/routes/facturas.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const facturasController = require('../controllers/facturasController');
-
-router.post('/', facturasController.crearFactura);
-router.get('/', facturasController.obtenerFacturas);
-
-module.exports = router;
